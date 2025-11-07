a334a1aa2e8303b3d309af8b5310ce44dd4fc297
Sean Ma <seanm3266@gmail.com>
2025-11-07 01:55:20 -0500

    Add fixed confidence interval stopping for problem 2

--- ps4_2.py
+++ ps4_2.py
@@ -1,7 +1,16 @@
+import math
+import time
+from collections import defaultdict
 from random import random
+from scipy.stats import t
+from matplotlib import pyplot as plt
 
 
-def findZeros(initial_state):
+def calcVar(sum_squares, sums, n):
+    return ((sum_squares - (pow(sums, 2) / n)) / (n - 1)) / n  # with bessel's correction
+
+
+def findZeros(initial_state=100):
     iterations = 1
     prev_state = initial_state
     while True:
@@ -23,6 +32,28 @@ def findZeros(initial_state):
     return iterations
 
 
-def simulate():
-    # track running variance?
-    pass
+def simulate(initial_state=1000, precision=1):
+    start_time = time.perf_counter()
+    running_sum = 0
+    sum_squares = 0
+    outcomes = defaultdict(int)
+    runs = 1
+    while True:
+        outcome = findZeros(initial_state)
+        outcomes[outcome] += 1
+        running_sum += outcome
+        sum_squares += outcome * outcome
+        # todo: go from fixed interval to percentage
+        if runs > 2 and t.ppf(0.975, runs-1) * math.sqrt(calcVar(sum_squares, running_sum, runs)) < precision:
+            break
+        runs += 1
+    print("Runs: " + str(runs))
+    print("Avg: " + str(running_sum / runs))
+    print("Simulation took " + str(time.perf_counter() - start_time) + " seconds")
+
+    plt.bar(outcomes.keys(), outcomes.values(), 1, color='g')
+    plt.title("Find Zeros Simulation")
+    plt.xlabel('Outcome')
+    plt.ylabel('Occurrences')
+    plt.show()
+    plt.close()
