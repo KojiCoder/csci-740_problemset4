4816d39f5b852cc5a233200b51ba7a49b8f58183
Sean Ma <seanm3266@gmail.com>
2025-11-07 01:28:26 -0500

    Fix probabilities for problem 4

--- ps4_2.py
+++ ps4_2.py
@@ -7,13 +7,15 @@ def findZeros(initial_state):
     while True:
         iterations += 1
 
-        if random() < 1 / prev_state:  # finding the zero with 1/j probability
+        u = random()
+        cum_prob = 1 / prev_state
+
+        if u < cum_prob:  # finding the zero with 1/j probability
             break
 
         # simulating k, the state of the current step
-        u = random()
         new_state = 1
-        cum_prob = 0
+
         while cum_prob < u:
             cum_prob += 2 * new_state / pow(prev_state, 2)
             new_state += 1
